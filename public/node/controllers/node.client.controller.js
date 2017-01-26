dcec25f3f6dc7002d78d56f2bec1a2a9fc4da064
andreafspeziale <andreaf.speziale@gmail.com>
2017-01-26 18:18:49 +0100

    added toastr messasges and working on node infos input

--- public/node/controllers/node.client.controller.js
+++ public/node/controllers/node.client.controller.js
@@ -1,8 +1,46 @@
 /**
  * Created by andreafspeziale on 25/01/17.
  */
-node.controller('NodeController', ['$scope',
-    ($scope) => {
+node.controller('NodeController', ['$scope', '$rootScope', 'toastr',
+    ($scope, $rootScope, toastr) => {
+        $scope.nodeIp = '';
+        $scope.nodePort = 8000;
+        /*
+            toDo | Check if data json already exists or not
 
+                true --> go to proper route
+                false --> go on with checks
+
+            toDo | Get node form input
+
+            toDo | check if node is reachable, and start creating a configuration json that will contain the configuration data.
+            {
+                node: {
+                    ip: 'ip',
+                    port: port
+                }
+            }
+
+            toDo | write the data and go to the next step/route
+        */
+
+        $scope.saveNode = () => {
+            if( !($scope.nodeIp == '' || $scope.nodePort == '' )) {
+
+                // valid values initialize the lib
+
+                const params = {
+                    host: $scope.nodeIp,
+                    port: $scope.nodePort,
+                    ssl: true
+                }
+
+                console.log(params);
+                // $rootScope.lisk = require('liskapi')();
+            } else {
+                console.log('Fill the fields');
+                toastr.warning('Fill all the fields', 'Warning');
+            }
+        }
 
     }])
--- public/node/node.client.module.js
+++ public/node/node.client.module.js
@@ -1,4 +1,4 @@
 /**
  * Created by andreafspeziale on 25/01/17.
  */
-const node = angular.module('node', []);
+const node = angular.module('node', ['ngAnimate', 'toastr']);
