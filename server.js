08d2be897bf374c69065577da1d521405654e305
Davide Gessa <gessadavide@gmail.com>
2017-01-30 10:58:48 +0100

    Routing and express

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 "use strict";
 
-var express     = require ('express');
+const express 		= require ('express');
+const lisk 			= require ('liskapi');
+const bodyParser 	= require('body-parser');
 
+const app = express ();
 
-var app = express ();
-app.use(express.static('.'));
+app.use (bodyParser.json({ type: 'application/*+json' }));
+app.use (express.static('.'));
+
+const router = express.Router (); 
+
+router.get('/:publicKey/accounts', (req, res) => {
+	lisk.getMultiSignatureAccounts ({ publicKey: req.params.publicKey }).call ()
+	.then ((res) => {
+		res.status (200);
+		res.json (res);
+	}).catch ((err) => {
+		res.status (500);
+		res.json (err);
+	});
+});
+
+app.use ('/api', router);
 
 app.listen (8082);
