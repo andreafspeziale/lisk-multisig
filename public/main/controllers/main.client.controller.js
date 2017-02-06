d093160023764f1b41bc55cb550b246339330d5d
andreafspeziale <andreaf.speziale@gmail.com>
2017-02-06 17:49:32 +0100

    added bootstrap ui on bower - run bower install - added modal for multisig creation

--- public/main/controllers/main.client.controller.js
+++ public/main/controllers/main.client.controller.js
@@ -1,7 +1,24 @@
 /**
  * Created by andreafspeziale on 03/02/17.
  */
-main.controller('MainController', ['$scope', '$http', '$location', '$rootScope', 'toastr',
-    ($scope, $http, $location, $rootScope, toastr) => {
+main.controller('MainController', ['$scope', '$http', '$location', '$rootScope', 'toastr', '$uibModal',
+    ($scope, $http, $location, $rootScope, toastr, $uibModal) => {
+
+        $scope.publicKeys = [''];
+
+        $scope.createMultisigModal = () => {
+            console.log("createMultisigModal");
+            $scope.modalInstance = $uibModal.open({
+                templateUrl: '/public/main/views/modals/createMultisigModal.html',
+                controller: 'MainController',
+                backdrop: 'static'
+            })
+        }
+
+        // toDo add dismiss modal
+
+        $scope.addPublicKey = () => {
+            $scope.publicKeys.push('');
+        }
 
     }]);
--- public/main/main.client.module.js
+++ public/main/main.client.module.js
@@ -1,4 +1,4 @@
 /**
  * Created by andreafspeziale on 03/02/17.
  */
-const main = angular.module('main', ['ngAnimate', 'toastr']);
+const main = angular.module('main', ['ngAnimate', 'toastr','ui.bootstrap']);
