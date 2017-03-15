e7a424cfcdbba2894547aa47f6e99857ef5520ba
andreafspeziale <andreaf.speziale@gmail.com>
2017-03-15 11:10:40 +0100

    fix /node and /wallet redirects

--- public/config/config/config.client.routes.js
+++ public/config/config/config.client.routes.js
@@ -10,4 +10,14 @@ configuration.config(['$routeProvider', '$locationProvider', ($routeProvider, $l
     }).otherwise({
         redirectTo: '/'
     });
+    $routeProvider.when('/node', {
+        templateUrl: '/public/config/views/config.html'
+    }).otherwise({
+        redirectTo: '/'
+    });
+    $routeProvider.when('/wallet', {
+        templateUrl: '/public/config/views/config.html'
+    }).otherwise({
+        redirectTo: '/'
+    });
 }])
